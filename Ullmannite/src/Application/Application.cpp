556cd55e6576426b9accdb26227042723bd36eb7
Abbion <amadeuszwalag@gmail.com>
2022-07-15 22:34:09 +0200

    GLFW added

--- Ullmannite/src/Application/Application.cpp
+++ Ullmannite/src/Application/Application.cpp
@@ -3,6 +3,8 @@
 
 #include "Logger/Logger.h";
 
+#include <GLFW/glfw3.h>
+
 using namespace Ull;
 
 Application::Application()
@@ -15,7 +17,40 @@ Application::~Application()
 	
 }
 
+static void key_callback(GLFWwindow* window, int key, int scancode, int action, int mods)
+{
+	if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
+		glfwSetWindowShouldClose(window, GLFW_TRUE);
+}
+
 void Application::Run()
 {
 	ULOGD("Hello");
+	ULOGF("Fatal error nr: " << 23);
+
+	GLFWwindow* window;
+
+	if (!glfwInit())
+		exit(EXIT_FAILURE);
+
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 2);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 0);
+
+	window = glfwCreateWindow(640, 480, "Simple example", NULL, NULL);
+	if (!window)
+	{
+		glfwTerminate();
+		exit(EXIT_FAILURE);
+	}
+
+	glfwMakeContextCurrent(window);
+
+	while (!glfwWindowShouldClose(window))
+	{
+		glfwPollEvents();
+	}
+
+	glfwDestroyWindow(window);
+
+	glfwTerminate();
 }
--- premake5.lua
+++ premake5.lua
@@ -9,6 +9,91 @@ workspace "Ullmannite"
 
 outputdir = "%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}"
 
+--====================================================================
+
+project "GLFW"
+    kind "StaticLib"
+    language "C"
+
+    targetdir("bin/" .. outputdir .. "/%{prj.name}")
+    objdir("bin-obj/" .. outputdir .. "/%{prj.name}")
+
+	files
+	{
+		"Ullmannite/ThirdPartyLibs/glfw/include/GLFW/glfw3.h",
+		"Ullmannite/ThirdPartyLibs/glfw/include/GLFW/glfw3native.h",
+		"Ullmannite/ThirdPartyLibs/glfw/src/glfw_config.h",
+		"Ullmannite/ThirdPartyLibs/glfw/src/context.c",
+		"Ullmannite/ThirdPartyLibs/glfw/src/init.c",
+		"Ullmannite/ThirdPartyLibs/glfw/src/input.c",
+		"Ullmannite/ThirdPartyLibs/glfw/src/monitor.c",
+        "Ullmannite/ThirdPartyLibs/glfw/src/platform.c",
+		"Ullmannite/ThirdPartyLibs/glfw/src/vulkan.c",
+		"Ullmannite/ThirdPartyLibs/glfw/src/window.c",
+        "Ullmannite/ThirdPartyLibs/glfw/src/egl_context.c",
+        "Ullmannite/ThirdPartyLibs/glfw/src/null_init.c",
+        "Ullmannite/ThirdPartyLibs/glfw/src/null_joystick.c",
+        "Ullmannite/ThirdPartyLibs/glfw/src/null_monitor.c",
+        "Ullmannite/ThirdPartyLibs/glfw/src/null_window.c"
+	}
+
+    filter "system:windows"
+		systemversion "latest"
+		staticruntime "On"
+
+		files
+		{
+			"Ullmannite/ThirdPartyLibs/glfw/src/win32_init.c",
+			"Ullmannite/ThirdPartyLibs/glfw/src/win32_joystick.c",
+			"Ullmannite/ThirdPartyLibs/glfw/src/win32_monitor.c",
+			"Ullmannite/ThirdPartyLibs/glfw/src/win32_time.c",
+			"Ullmannite/ThirdPartyLibs/glfw/src/win32_thread.c",
+			"Ullmannite/ThirdPartyLibs/glfw/src/win32_window.c",
+            "Ullmannite/ThirdPartyLibs/glfw/src/win32_module.c",
+			"Ullmannite/ThirdPartyLibs/glfw/src/wgl_context.c",
+			"Ullmannite/ThirdPartyLibs/glfw/src/osmesa_context.c"
+		}
+
+		defines 
+		{ 
+			"_GLFW_WIN32",
+			"_CRT_SECURE_NO_WARNINGS"
+		}
+
+	filter "system:linux"
+		pic "On"
+
+		systemversion "latest"
+		staticruntime "On"
+
+        files
+        {
+			"Ullmannite/ThirdPartyLibs/glfw/src/x11_init.c",
+			"Ullmannite/ThirdPartyLibs/glfw/src/x11_monitor.c",
+			"Ullmannite/ThirdPartyLibs/glfw/src/x11_window.c",
+			"Ullmannite/ThirdPartyLibs/glfw/src/xkb_unicode.c",
+			"Ullmannite/ThirdPartyLibs/glfw/src/posix_time.c",
+			"Ullmannite/ThirdPartyLibs/glfw/src/posix_thread.c",
+			"Ullmannite/ThirdPartyLibs/glfw/src/glx_context.c",
+			"Ullmannite/ThirdPartyLibs/glfw/src/osmesa_context.c",
+			"Ullmannite/ThirdPartyLibs/glfw/src/linux_joystick.c"
+        }
+
+        defines
+		{
+			"_GLFW_X11"
+		}
+
+    filter "configurations:Debug"
+		runtime "Debug"
+		symbols "on"
+
+	filter "configurations:Release"
+		runtime "Release"
+		optimize "on"
+
+--====================================================================
+
 project "Ullmannite"
     location  "Ullmannite"
     kind "ConsoleApp"
@@ -29,7 +114,14 @@ project "Ullmannite"
     includedirs
     {
         "%{prj.name}/src",
-        "%{prj.name}/ThirdPartyLibs/plog/include"
+        "%{prj.name}/ThirdPartyLibs/plog/include",
+        "%{prj.name}/ThirdPartyLibs/glfw/include"
+    }
+
+    links
+    {
+        "GLFW",
+        "opengl32.lib"
     }
 
 
@@ -51,5 +143,5 @@ project "Ullmannite"
         defines "RELEASE"
         optimize "On"
 
---    filter {"system:windows", "configuration:Release"}
---        buildoptions "/MT"
+--====================================================================
+
