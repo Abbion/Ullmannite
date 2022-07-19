b7bfbd261076af39ce4966589074ec6556a920bd
Abbion <amadeuszwalag@gmail.com>
2022-07-19 20:58:05 +0200

    Premake chmod

--- BuildScripts/UbuntuBuild.sh
+++ BuildScripts/UbuntuBuild.sh
@@ -4,5 +4,6 @@ mv -v Ullmannite/ThirdPartyLibs/imgui/backends/imgui_impl_opengl3.h Ullmannite/T
 mv -v Ullmannite/ThirdPartyLibs/imgui/backends/imgui_impl_opengl3.cpp Ullmannite/ThirdPartyLibs/imgui
 mv -v Ullmannite/ThirdPartyLibs/imgui/backends/imgui_impl_glfw.h Ullmannite/ThirdPartyLibs/imgui
 mv -v Ullmannite/ThirdPartyLibs/imgui/backends/imgui_impl_glfw.cpp Ullmannite/ThirdPartyLibs/imgui
+chmod +x ThirdParty/bin/premake/LinuxPremake5
 ./ThirdParty/bin/premake/LinuxPremake5 gmake2
 make
