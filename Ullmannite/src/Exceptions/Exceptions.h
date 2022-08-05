dff391f8254787c9e5e6c6a6dbb0eab24881ce0f
Abbion <amadeuszwalag@gmail.com>
2022-08-05 21:09:58 +0200

    Linux exception fix 2

--- Ullmannite/src/Exceptions/Exceptions.h
+++ Ullmannite/src/Exceptions/Exceptions.h
@@ -7,7 +7,7 @@ namespace Ull
 	public:
 		InitializationException(const char* str) : m_message(str) {}
 
-		const char* what() const override {
+		const char* what() const override noexcept {
 			return m_message;
 		}
 
